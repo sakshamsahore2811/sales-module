596245401a85539720e4916294cceb2585981dbc
Saksham Sharma <201382@juitsolan.in>
2023-11-22 10:39:25 +0530

    first commit

---
+++ src/app/leaderboard/page.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Styles from "./page.module.css";
+import SideBar from "../../../components/SideBar/page";
+import MyLeaderboard from "./components/MyLeaderboard/page";
+
+const page = () => {
+  return (
+    <div className={`${Styles.parent}`}>
+      <div className={`${Styles.SideBar}`}>
+        <SideBar />
+      </div>
+      <div className={`${Styles.dashboard}`}>
+        <MyLeaderboard />
+      </div>
+    </div>
+  );
+};
+
+export default page;
